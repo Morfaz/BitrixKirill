b2a04b30ca7b3c5551a8abc24939f5a8b3ff44bb
Morfaz <iceskill2002@mail.ru>
2022-06-17 11:08:21 +0500

    Задание 4 О таксопарке

--- www/index.php
+++ www/index.php
@@ -5,9 +5,7 @@ use Sprint\Options\Builder\Builder;
 $APPLICATION->SetPageProperty("title", "Главная");
 $APPLICATION->SetPageProperty("description", "Главная страница");
 $APPLICATION->SetTitle("General");
-?>
-
-<?$APPLICATION->IncludeComponent(
+?><?$APPLICATION->IncludeComponent(
 	"bitrix:news.list",
 	"mainpage_top_slider",
 	Array(
@@ -23,6 +21,7 @@ $APPLICATION->SetTitle("General");
 		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "N",
 		"CHECK_DATES" => "Y",
+		"COMPONENT_TEMPLATE" => "mainpage_top_slider",
 		"DETAIL_URL" => "",
 		"DISPLAY_BOTTOM_PAGER" => "N",
 		"DISPLAY_DATE" => "N",
@@ -30,10 +29,10 @@ $APPLICATION->SetTitle("General");
 		"DISPLAY_PICTURE" => "Y",
 		"DISPLAY_PREVIEW_TEXT" => "N",
 		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array("NAME","PREVIEW_TEXT","PREVIEW_PICTURE","DETAIL_TEXT",""),
+		"FIELD_CODE" => array(0=>"NAME",1=>"PREVIEW_TEXT",2=>"PREVIEW_PICTURE",3=>"DETAIL_TEXT",4=>"",),
 		"FILTER_NAME" => "",
 		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "",
+		"IBLOCK_ID" => "5",
 		"IBLOCK_TYPE" => "content",
 		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
 		"INCLUDE_SUBSECTIONS" => "N",
@@ -49,7 +48,7 @@ $APPLICATION->SetTitle("General");
 		"PARENT_SECTION" => "",
 		"PARENT_SECTION_CODE" => "",
 		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array("","link",""),
+		"PROPERTY_CODE" => array(0=>"",1=>"link",2=>"",),
 		"SET_BROWSER_TITLE" => "N",
 		"SET_LAST_MODIFIED" => "N",
 		"SET_META_DESCRIPTION" => "N",
@@ -64,11 +63,84 @@ $APPLICATION->SetTitle("General");
 		"STRICT_SECTION_CHECK" => "N"
 	)
 );?>
-
+    <?$APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	"about_taxi", 
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "N",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"DISPLAY_DATE" => "N",
+		"DISPLAY_NAME" => "N",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "N",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(
+			0 => "NAME",
+			1 => "PREVIEW_TEXT",
+			2 => "PREVIEW_PICTURE",
+			3 => "",
+		),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "7",
+		"IBLOCK_TYPE" => "content",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "N",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "5",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ID",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "ASC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N",
+		"COMPONENT_TEMPLATE" => "about_taxi"
+	),
+	false
+);?>
     <script>
     addEventListener("scroll",function(){
         var page=window.pageYOffset;
         var site_header=document.querySelector(".site-header");
-        10<page?(site_header.classList.add("shadow")):(
-            site_header.classList.remove("shadow"))});
+        if (10 < page){
+            site_header.classList.add("shadow");
+            site_header.classList.remove("off-stick");
+
+        }else{
+            site_header.classList.remove("shadow");
+            site_header.classList.add("off-stick");
+        }
+        });
 </script><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
